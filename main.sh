8303fccb6d0e73ebac2dc177b98ed16f64ccbb06
FrostyNick <57016218+FrostyNick@users.noreply.github.com>
2024-12-17 21:21:03 -0600

    Update main.sh + add template.html

--- main.sh
+++ main.sh
@@ -1,13 +1,16 @@
-# pandoc_notes.md for instructions
+#!/usr/bin/env bash
+# Best way to use this at the moment:
+# - In one terminal, run `./main.sh serve` (If you run it for a while, consider running your terminal using `abduco` (basically a `tmux` session) so you can close your terminal.)
+# - In another terminal run live-server (or using neovim live-server as setup in my dotfiles, <leader>ll).
 
 update_html() {
-	chmod +w index.html && pandoc --template=default.html index.md -c style.css -o index.html
+	chmod +w index.html && pandoc index.md -o index.html --template=template.html
 	chmod -w index.html
 }
 update_html
 
-if [[ $1 == "serve" || $2 == "serve" ]]
-then
+# Disclaimer: "serve" and "auto" don't work at the same time.
+if [[ $1 == "serve" || $2 == "serve" ]]; then
 	python -m http.server -b localhost &
 	P1=$!
 elif [[ $1 == "auto" || $2 == "auto" ]]; then
